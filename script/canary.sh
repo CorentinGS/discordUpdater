7b3e54f4e41c54bae08ec244a662a31a5ed321c3
CorentinGS <c.giaufersaubert@outlook.com>
2023-01-14 17:08:33 +0100

    :bug: Embed

--- script/canary.sh
+++ script/canary.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-cd ./installer || exit
 mkdir -p ~/.local/bin
 mkdir ./tmp
+cd ./tmp || exit
 echo "Downloading canary..."
 wget "https://discordapp.com/api/download/canary?platform=linux&format=tar.gz" -O ./tmp/discord-canary.tar.gz
 tar -xvf ./tmp/discord-canary.tar.gz -C ~/.local/bin
 echo "Installing canary..."
+sudo rm /usr/bin/DiscordCanary || true # Remove old symlink
 sudo ln -s ~/.local/bin/DiscordCanary/discord.png /usr/share/icons/discord-canary.png
 sudo ln -s ~/.local/bin/DiscordCanary/DiscordCanary /usr/bin
 mkdir -p ~/.local/share/applications/
-cp installer/discord-canary.desktop ~/.local/share/applications/
+wget https://raw.githubusercontent.com/CorentinGS/discordUpdater/main/assets/discord-canary.desktop -O ~/.local/share/applications/discord-canary.desktop
+sudo rm /usr/share/applications/discord-canary.desktop || true # Remove old symlink
 sudo ln -s ~/.local/share/applications/discord-canary.desktop /usr/share/applications/discord-canary.desktop
 echo "Cleaning up..."
-rm -rf ./tmp
+cd ../ && rm -rf ./tmp
 echo "Done!"
 exit 0
--- script/ptb.sh
+++ script/ptb.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-cd ./installer || exit
 mkdir -p ~/.local/bin
 mkdir ./tmp
+cd ./tmp || exit
 echo "Downloading discord PTB..."
-wget "https://ptb.discord.com/api/download?platform=linux&format=tar.gz" -O ./tmp/discord-ptb.tar.gz
-tar -xvf ./tmp/discord-ptb.tar.gz -C ~/.local/bin
+wget "https://discord.com/api/download/ptb?platform=linux&format=tar.gz" -O discord-ptb.tar.gz
+tar -xvf discord-ptb.tar.gz -C ~/.local/bin
 echo "Installing discord PTB..."
+sudo rm /usr/bin/DiscordPTB || true # Remove old symlink
 sudo ln -s ~/.local/bin/DiscordPTB/discord.png /usr/share/icons/discord-ptb.png
 sudo ln -s ~/.local/bin/DiscordPTB/DiscordPTB /usr/bin
 mkdir -p ~/.local/share/applications/
-cp installer/discord-ptb.desktop ~/.local/share/applications/
+wget https://raw.githubusercontent.com/CorentinGS/discordUpdater/main/assets/discord-ptb.desktop -O ~/.local/share/applications/discord-ptb.desktop
+sudo rm /usr/share/applications/discord-ptb.desktop || true # Remove old symlink
 sudo ln -s ~/.local/share/applications/discord-ptb.desktop /usr/share/applications/discord-ptb.desktop
 echo "Cleaning up..."
-rm -rf ./tmp
+cd ../ && rm -rf ./tmp
 echo "Done!"
 exit 0
